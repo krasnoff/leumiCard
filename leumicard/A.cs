38e1ebac83f6a8e727e4cf410ed6b39f368610c5
krasnof.kobi@gmail.com <krasnoff.kobi@gmail.com>
2018-10-08 20:00:53 +0300

    Add project files.

---
+++ leumicard/A.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leumicard
+{
+    class A
+    {
+        private B b;
+
+        public A()
+        {
+            b = new B();
+            b.CoffeeReady += new CoffeeReadyEventHandler(callFriend);
+        }
+
+        public void run()
+        {
+            Debug.WriteLine("A.run");
+            b.makeCoffee();
+        }
+
+        public void callFriend(object sender)
+        {
+            Debug.WriteLine("A.callFriend");
+        }
+    }
+}
---
+++ leumicard/B.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace leumicard
+{
+    public delegate void CoffeeReadyEventHandler(object sender);
+
+    class B
+    {
+        public event CoffeeReadyEventHandler CoffeeReady;
+
+        public void makeCoffee()
+        {
+            Debug.WriteLine("B.makeCoffee");
+            Thread.Sleep(1000);
+            OnCoffeeReady(); // start raise event
+            
+        }
+
+        protected virtual void OnCoffeeReady()
+        {
+            CoffeeReady?.Invoke(this);//Raise the event
+        }
+    }
+}
